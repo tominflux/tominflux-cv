57a111c892f28fd6b52b7ca2a4cddb6641adad7b
Tom Chesters <tom@tomaaato.xyz>
2024-04-18 13:39:18 +0100

    Implement rough `CvList` component

---
+++ src/app/components/CvList/index.tsx
@@ -0,0 +1,31 @@
+import { ReactNode } from "react";
+import { CvHeading, CvHeadingProps } from "../CvHeading";
+
+export interface CvListProps {
+  margin?: number;
+  heading?: ReactNode;
+  headingProps?: Omit<CvHeadingProps, "children">;
+  items: ReactNode[];
+}
+
+export function CvList({
+  margin = 2,
+  heading,
+  headingProps = {
+    type: "h3",
+    size: "xl",
+    margin: 1,
+  },
+  items,
+}: CvListProps) {
+  return (
+    <div className={`mb-${margin}`}>
+      {heading ? <CvHeading {...headingProps}>{heading}</CvHeading> : undefined}
+      <ul className="list-disc pl-6">
+        {items.map((item) => (
+          <li className="pl-1">{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
