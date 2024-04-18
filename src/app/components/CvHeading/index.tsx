971e07fd084dc060b8c1a2eece4296075ed65a6e
Tom Chesters <tom@tomaaato.xyz>
2024-04-18 13:26:28 +0100

    Implement `CvHeading` component

---
+++ src/app/components/CvHeading/index.tsx
@@ -0,0 +1,23 @@
+import { HeadingType } from "@/app/types/HeadingType";
+import { TextSize } from "@/app/types/TextSize";
+import React from "react";
+import { ReactNode } from "react";
+
+export interface CvHeadingProps {
+  type: HeadingType;
+  margin?: number;
+  size?: TextSize;
+  children: ReactNode;
+}
+
+export function CvHeading({
+  type,
+  margin = 2,
+  size = "3xl",
+  children,
+}: CvHeadingProps) {
+  return React.createElement(type, {
+    className: `text-${size} mb-${margin}`,
+    children,
+  });
+}
