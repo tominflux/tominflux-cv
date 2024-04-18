df1e14d6f4f1f3f0e340780dc582231bb23ff40c
Tom Chesters <tom@tomaaato.xyz>
2024-04-18 13:50:09 +0100

    Add `heading` feature to `CvSection`

--- src/app/components/CvSection/index.tsx
+++ src/app/components/CvSection/index.tsx
@@ -1,10 +1,29 @@
 import { ReactNode } from "react";
+import { CvHeading, CvHeadingProps } from "../CvHeading";
 
 export interface CvSectionProps {
   margin?: number;
+  heading?: ReactNode;
+  headingProps?: Omit<CvHeadingProps, "children">;
+  padding?: number;
   children: ReactNode;
 }
 
-export function CvSection({ margin = 2, children }: CvSectionProps) {
-  return <div className={`w-full mb-${margin}`}>{children}</div>;
+export function CvSection({
+  margin = 2,
+  heading,
+  headingProps = {
+    type: "h2",
+    size: "2xl",
+    margin: 2,
+  },
+  padding = 3,
+  children,
+}: CvSectionProps) {
+  return (
+    <div className={`w-full mb-${margin}`}>
+      {heading ? <CvHeading {...headingProps}>{heading}</CvHeading> : undefined}
+      <div className={`px-${padding}`}>{children}</div>
+    </div>
+  );
 }
